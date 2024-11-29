9d17fd9a81c512eda4b0c5d0a56fef5e5924c1c4
Bamas Angkasa <inilobamas@gmail.com>
2024-11-29 11:16:02 +0700

    dropdown handler

--- domain/domain_email/handler.go
+++ domain/domain_email/handler.go
@@ -2,22 +2,20 @@ package domain
 
 import (
 	"email-platform/config"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func GetDropdownDomainHandler(c echo.Context) error {
-	// Fetch email details by ID
-	var domain []DomainEmail
-	err := config.DB.Get(&domain, `SELECT 
-			domain,
-            created_at, 
-            updated_at 
-			FROM domains`)
+	// Fetch all domains
+	var domains []DomainEmail
+	err := config.DB.Select(&domains, `SELECT id, domain, created_at, updated_at FROM domains`)
 	if err != nil {
-		return c.JSON(http.StatusNotFound, map[string]string{"error": "Domain not found"})
+		fmt.Println("error fetching domains", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch domains"})
 	}
 
-	return c.JSON(http.StatusOK, domain)
+	return c.JSON(http.StatusOK, domains)
 }
