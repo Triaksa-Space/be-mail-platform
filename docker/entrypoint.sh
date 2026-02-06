10ab9082953c01f79691d31db096501f157bdb50
Bamas Angkasa <inilobamas@gmail.com>
2026-02-06 23:04:07 +0700

    lfg

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = "server" ]; then
   echo "Running migrations..."
-  goose -dir /app/migrations mysql "$DATABASE_URL" up
+  goose -dir /app/migrations mysql "mailria_user:mailriauserpassword@tcp(mysql:3306)/mailria?parseTime=true" up
 fi
 
 exec ./mailria "$@"
