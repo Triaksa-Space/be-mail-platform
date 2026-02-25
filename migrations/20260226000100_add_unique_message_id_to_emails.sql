bb0855a26382105e9fc9f46f7fe9bbd6fe259f02
Bamas Angkasa <inilobamas@gmail.com>
2026-02-26 02:56:39 +0700

    err migration

--- migrations/20260226000100_add_unique_message_id_to_emails.sql
+++ migrations/20260226000100_add_unique_message_id_to_emails.sql
@@ -1,7 +1,5 @@
 -- +goose Up
 -- +goose StatementBegin
--- Remove any existing duplicate (user_id, message_id) pairs before adding the constraint.
--- Keep the oldest row (lowest id) for each duplicate group.
 DELETE e1
 FROM emails e1
 INNER JOIN emails e2
@@ -9,7 +7,9 @@ INNER JOIN emails e2
     AND e1.message_id = e2.message_id
     AND e1.message_id IS NOT NULL
     AND e1.id > e2.id;
+-- +goose StatementEnd
 
+-- +goose StatementBegin
 ALTER TABLE emails
     ADD UNIQUE KEY uq_emails_user_message (user_id, message_id);
 -- +goose StatementEnd
