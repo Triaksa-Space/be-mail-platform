4762c15ecb535272613f8b8a7616ec34747aeec7
Bamas Angkasa <inilobamas@gmail.com>
2026-02-27 09:35:04 +0700

    staging harus kesini db nya

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = "server" ]; then
   echo "Running migrations..."
-  goose -dir /app/migrations mysql "mailria_prod:aa!ma1Lr!a@tcp(db-mailria-prod.c9yag8g0s8mo.ap-southeast-1.rds.amazonaws.com:3306)/mailsaja_dev?parseTime=true" up
+  goose -dir /app/migrations mysql "mailria_user:mailriauserpassword@tcp(mysql:3306)/mailria?parseTime=true" up
 fi
 
 exec ./mailria "$@"
