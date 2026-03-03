63b440ec2cb30117dd708278346d92a3e9b258b3
Bamas Angkasa <inilobamas@gmail.com>
2026-03-03 13:18:10 +0700

    fix

--- routes/route.go
+++ routes/route.go
@@ -81,7 +81,7 @@ func RegisterRoutes(e *echo.Echo) {
 	emailGroup.POST("/by_user/download/file", email.GetFileEmailToDownloadHandler)
 	emailGroup.GET("/by_user/:id", email.ListEmailByIDHandler, middleware.RoleMiddleware(adminRoles))
 	emailGroup.GET("/sent/by_user", email.SentEmailByIDHandler)
-	emailGroup.GET("/sent/by_user/:id", email.ListSentEmailsByUserIDHandler, middleware.RoleMiddleware(adminRoles), middleware.AdminPermissionMiddleware("all_sent")) // Admin: list sent emails by user
+	emailGroup.GET("/sent/by_user/:id", email.ListSentEmailsByUserIDHandler, middleware.RoleMiddleware(adminRoles), middleware.AdminPermissionMiddleware("user_list")) // Admin: list sent emails by user (from User List page)
 	emailGroup.GET("/sent/list", email.GetUserSentEmailsHandler)                                                                                                      // User's own sent emails list
 	emailGroup.GET("/sent/detail/:id", email.GetUserSentEmailDetailHandler)                                                                                           // User's own sent email detail
 	emailGroup.GET("/sent", email.GetUserSentEmailsHandler)                                                                                                           // User's own sent emails
